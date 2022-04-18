447017c068984f2560302604afa6cfe7dad7b52d
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-04-18 23:55:32 +0600

    updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="http://demo1.wpopal.com/grenda/wp-content/uploads/revslider/slider1/slider1.jpg"
+            src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="First slide"
           />
           <Carousel.Caption>
@@ -20,17 +20,15 @@ const Banner = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="http://demo1.wpopal.com/grenda/wp-content/uploads/revslider/slider1/slider2.jpg"
+            src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="Second slide"
           />
 
           <Carousel.Caption>
-          <h1>Party</h1>
+            <h1>Party</h1>
             <p>Let's plan your next event together</p>
           </Carousel.Caption>
         </Carousel.Item>
-        
-    
       </Carousel>
     </div>
   );
