5e7125ee6307d44a3f374947918989227fbe4358
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-04-18 22:29:23 +0600

    form updated

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -10,7 +10,7 @@ const CheckOut = () => {
     }
     return (
         <div className="container w-50 mx-auto">
-            <h1 className="text-info text-center">Please Submit this form</h1>
+            <h1 className="text-dark text-center">Please Submit this form</h1>
             <form onSubmit={handleSubmit}>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Username</label>
@@ -24,7 +24,7 @@ const CheckOut = () => {
     <label for="exampleInputPassword1" class="form-label">Address</label>
     <input type="text" class="form-control" id="exampleInputPassword1"/>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-dark">Submit</button>
 </form>
         </div>
     );
